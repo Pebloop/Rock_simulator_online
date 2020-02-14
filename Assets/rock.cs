f7fef9fa0c7358973aaf2bcc416626487f489a34
TempoDev <alexleg@live.fr>
2020-02-14 14:45:07 +0100

    init project

---
+++ Assets/rock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Random.Range(-20, 20);
+        pos.z = Random.Range(-20, 20);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
